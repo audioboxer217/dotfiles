e1057979ecff274584c96613d925e094d9f50719
Scott Eppler <seppler@presidio.com>
2022-09-21 11:56:05 -0500

    Move run_tests.sh so it doesn't run via chezmoi

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-# Run this file to run all the tests, once
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
-"${DIR}"/test/libs/bats/bin/bats "${DIR}"/test/*.bats
