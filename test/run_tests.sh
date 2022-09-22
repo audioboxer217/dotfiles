c891539756bb61ec8efea2034363f53367139480
Scott Eppler <seppler@presidio.com>
2022-09-22 16:01:37 -0500

    Add `git submodule update --init` to 'run_tests.sh'

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# Ensure submodules are in place and up-to-date
+git submodule update --init
+
 # Run this file to run all the tests, once
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 "${DIR}"/libs/bats/bin/bats "${DIR}"/*.bats
