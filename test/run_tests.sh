4898aa4a8c77d85f5fb645b2d8cf26c775b9165e
Scott Eppler <seppler@presidio.com>
2024-09-19 10:53:52 -0500

    Update 'run_tests.sh' to generate Summary

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,6 +4,30 @@ set -eo pipefail
 # Ensure submodules are in place and up-to-date
 git submodule update --init
 
-# Run this file to run all the tests, once
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-"${DIR}"/bats/bin/bats "${DIR}"/*.bats | tee "${GITHUB_STEP_SUMMARY-test.log}"
+mkdir "${DIR}"/output
+
+"${DIR}"/bats/bin/bats --pretty --report-formatter tap --output "${DIR}"/output "${DIR}"/*.bats
+
+echo "# BATS Summary" > "${GITHUB_STEP_SUMMARY-test.md}"
+echo "|Name|Result|" >> "${GITHUB_STEP_SUMMARY-test.md}"
+echo "|---|---|" >> "${GITHUB_STEP_SUMMARY-test.md}"
+{
+  read
+  while read line; do
+    read -r -a test_arr <<< "$line"
+    if [[ "${test_arr[0]}" == 'ok' ]]
+    then
+      status="✅"
+      name=("${test_arr[@]:2}")
+    else
+      status="❌"
+      name=("${test_arr[@]:3}")
+    fi
+
+    echo "${name[@]}|${status}" >> "${GITHUB_STEP_SUMMARY-test.md}"
+  done 
+} <"${DIR}"/output/report.tap
+
+rm -rf "${DIR}"/output
